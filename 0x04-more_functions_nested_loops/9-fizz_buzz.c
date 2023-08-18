fc0986d8e662e21b25c4e29616da860e613c05eb
Donjosh07 <onyendijoshua@gmail.com>
2023-08-17 21:33:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 	{
 	if (i % 3 == 0 && i % 5 != 0)
 	{
-	printf(" Fizz");
+	printf("Fizz");
 	}
 	else if (i % 5 == 0 && i % 3 != 0)
 	{
-	printf(" Buzz");
+	printf("Buzz");
 	}
 	else if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf(" FizzBuzz");
+	printf("FizzBuzz");
 	}
 	else if (i == 1)
 	{
@@ -35,7 +35,6 @@ int main(void)
 	}
 	printf("\n");
 	}
-
 	return (0);
 }
 
